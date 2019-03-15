329312c5bd4b9b7e183413ca60ed5afdc1774998
MariusHendriks <Mark.hendriks9999@gmail.com>
2019-03-15 12:57:30 +0100

    logout possible!

--- app/src/main/java/com/maxaltena/socialkit/MainActivity.java
+++ app/src/main/java/com/maxaltena/socialkit/MainActivity.java
@@ -3,6 +3,9 @@ package com.maxaltena.socialkit;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.FirebaseApp;
@@ -65,4 +68,24 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
+    //Replaces menu with main_menu.xml
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    //logout thing
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.sign_out_menu:
+                //Sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
