95dbfd31b0764cc3a1855705eb588ea89f48175d
MariusHendriks <Mark.hendriks9999@gmail.com>
2019-03-15 12:47:52 +0100

    Logging in with Google and email possible!

--- app/src/main/java/com/maxaltena/socialkit/MainActivity.java
+++ app/src/main/java/com/maxaltena/socialkit/MainActivity.java
@@ -1,13 +1,68 @@
 package com.maxaltena.socialkit;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Initialize global vars
+    public static final int RC_SIGN_IN = 1;
+
+    // Global firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        // Initialize Firebase Components
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        //Reference and listeners
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Get user
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                //Check if user is logged in
+                if(user!= null){
+                    //user is signed in
+                } else {
+                    //user is not signed in
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
